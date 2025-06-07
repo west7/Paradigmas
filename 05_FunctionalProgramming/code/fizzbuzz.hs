f76e6e535d7d40550f409c4e4224bff96329b715
Guilherme Westphall <guilhermewestphall@gmail.com>
2025-06-06 22:07:00 -0300

    idiomatic impl

--- 05_FunctionalProgramming/code/fizzbuzz.hs
+++ 05_FunctionalProgramming/code/fizzbuzz.hs
@@ -6,3 +6,12 @@ fizzbuzz n =
         else if mod x 3 == 0 then "Fizz" : acc 
         else if mod x 5 == 0 then "Buzz" : acc
             else show x : acc) [] list 
+
+
+fizzbuzz2 :: (Integral a, Show a) => a -> [String]
+fizzbuzz2 n = map fizz [1..n]  where
+    fizz x 
+        | x `mod` 15 == 0 = "FizzBuzz"
+        | x `mod` 3 == 0  = "Buzz"
+        | x `mod` 5 == 0  = "Fizz"
+        | otherwise       = show x
